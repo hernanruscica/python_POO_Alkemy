162aa22955ad1b3fb24abe0347cfb42bc9f2f7c7
hernanruscica <cesarhernanruscica@gmail.com>
2024-02-23 10:59:12 -0300

    terminado el ejercio de objetos de animales

--- Ejercicios WorkTime resueltos/unidad 2/ejercio2.py
+++ Ejercicios WorkTime resueltos/unidad 2/ejercio2.py
@@ -10,6 +10,8 @@ class Animal:
         self.__vertebrado = tipo_animal
     def comer(self):
         return "estoy comiendo"
+    def emitir_sonido(self):
+        return print("estoy emitiendo mi sonido caracteristico ")
     
 class Perro(Animal):
     def __init__(self, cant_patas, tipo_animal, nombre, raza):
@@ -18,8 +20,25 @@ class Perro(Animal):
         self.__raza = raza
     def correr(self):
         return "estoy corriendo"
+    def emitir_sonido(self):
+        return print("Soy %s y estoy ladrando" % self.__nombre)
+
+class Aguila(Animal):
+    def __init__(self, cant_patas, tipo_animal, nombre, raza):
+        super().__init__(cant_patas, tipo_animal)
+        self.__nombre= nombre
+        self.__raza = raza
+    def volar(self):        
+        mensaje = print("%s dice: 'estoy volando'" % self.__nombre)
+        return mensaje
+    
+        
+            
     
 pichichuz = Perro(4, TipoAnimal.VERTEBRADO, "Pichichuz", "callejero")
+Ricardo = Aguila(2, TipoAnimal.VERTEBRADO, "Ricardo", "Ave")
+#print(pichichuz.correr())    
+Ricardo.volar()
+pichichuz.emitir_sonido()
+Ricardo.emitir_sonido()
 
-
-    
