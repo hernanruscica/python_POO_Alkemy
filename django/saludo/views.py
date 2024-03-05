36ecb765a78f9878af45b10d1b528de437b29ea9
hernanruscica <cesarhernanruscica@gmail.com>
2024-03-05 20:53:07 -0300

    hola mundo con django

---
+++ django/saludo/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+# Create your views here.
+
+from django.http import HttpResponse
+
+def hola_mundo(request):
+    return HttpResponse("¡Hola Mundo!")
+
+def mostrar_nombre(request):
+    nombre = 'Cesar Hernan Ruscica'  # Puedes cambiar esto por tu nombre
+    return render(request, 'mostrar_nombre.html', {'nombre': nombre})
+
