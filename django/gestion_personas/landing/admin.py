e4d5d538a1f8715cf78bd0e17842d4c07966977e
hernanruscica <cesarhernanruscica@gmail.com>
2024-03-14 20:48:43 -0300

    empezando con django admin

--- django/gestion_personas/landing/admin.py
+++ django/gestion_personas/landing/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Estudiante
+
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido', 'edad', 'nota']
+    search_fields = ['apellido']
+
 
 # Register your models here.
+
+admin.site.register(Estudiante, EstudianteAdmin)
