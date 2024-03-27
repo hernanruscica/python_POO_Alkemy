0b4e4a18364ee81457b5b0f2514b7a5c72526538
hernanruscica <cesarhernanruscica@gmail.com>
2024-03-27 12:02:25 -0300

    agregue el admin a django, vista de producto, listados, creacion de ambos

--- gestion_personas_tp/StockControl/Sales/models.py
+++ gestion_personas_tp/StockControl/Sales/models.py
@@ -2,14 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Proveedor(models.Model):
-    nombre = models.CharField(max_length = 45)
-    apellido = models.CharField(max_length = 45) 
-    dni = models.PositiveIntegerField()
+    razon_social = models.CharField(max_length = 45)
+    cuit = models.CharField(max_length = 45) 
+    celular = models.CharField(max_length = 45) 
+    def __str__(self):
+        return self.razon_social
+    
 
 class Producto(models.Model):
     nombre = models.CharField(max_length = 45)
     precio = models.FloatField()
     stock_actual = models.IntegerField()
     proveedor = models.ForeignKey(Proveedor, on_delete = models.CASCADE)
-
+    def __str__(self):
+        return self.nombre
 
--- gestion_personas_tp/StockControl/Sales/views.py
+++ gestion_personas_tp/StockControl/Sales/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Producto, Proveedor
 
@@ -7,10 +7,12 @@ def test(request):
     proveedorUNO = Proveedor.objects.get(pk=1)
     #prueba = Producto.objects.create(nombre = "lamparita", precio = 100, stock_actual = 10, proveedor = proveedorUNO)
     prueba = Proveedor.objects.create(nombre = "Pedro", apellido = "Gonzalez", dni = 32456789)
+    print(proveedorUNO)
     return HttpResponse(prueba.nombre)
 
-def root(request):
-    return HttpResponse('Root')
+def home(request):
+    return render(request, 'home.html')
+    #return HttpResponse('Root')
 
 def new_supplier_form(request):
     return render(request, 'new_supplier_form.html')
@@ -22,14 +24,15 @@ def new_product_form(request):
 
 def create_supplier(request):
     if request.method == 'POST':
-        nombre = request.POST.get('nombre')
-        apellido = request.POST.get('apellido')
-        dni = request.POST.get('dni')
+        razon_social = request.POST.get('razon_social')
+        cuit = request.POST.get('cuit')
+        celular = request.POST.get('celular')
 
         # Crear instancia del proveedor y guardar en la base de datos
-        proveedor = Proveedor(nombre=nombre, apellido=apellido, dni=dni)
-        proveedor.save()      
-        return HttpResponse('proveedor insertado en la BD')
+        proveedor = Proveedor(razon_social=razon_social, cuit=cuit, celular=celular)
+        proveedor.save()    
+        return redirect('suppliers')  
+        #return HttpResponse('proveedor insertado en la BD')
     
 def create_product(request):
     if request.method == 'POST':
@@ -42,11 +45,24 @@ def create_product(request):
         #print(proveedor.nombre)
         producto = Producto(nombre = nombre, precio = precio, stock_actual = stock_actual, proveedor = proveedor)
         producto.save()
-        return HttpResponse('producto "%s" insertado en la BD' % producto.nombre)
+        return redirect('products')
+        #return HttpResponse('producto "%s" insertado en la BD' % producto.nombre)
 
 def products(request):    
     print('ver todos los Products')
     products = Producto.objects.all()   
+    #print(products[0].proveedor)
     return render(request, 'productos.html', {'productos': products})
+
+def view_product(request, id):
+    print('view product with id %d' % id)
+    producto = Producto.objects.get(id=id)
+    #print(producto.nombre)
+    return render(request, 'view_products.html', {'producto': producto})
+    #return HttpResponse('view product with id %d' % id)
+
 def suppliers(request):
-    return HttpResponse('suppliers')
+    print('ver todos los proveedores')
+    proveedores = Proveedor.objects.all()   
+    #return HttpResponse('suppliers')
+    return render(request, 'proveedores.html', {'proveedores': proveedores})
